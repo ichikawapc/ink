de9fe7565635f6763e592f989d019029acc228b1
ringospoon <116368810+ringospoon@users.noreply.github.com>
2023-06-04 13:27:34 +0900

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,22 @@
-object Main extends App {
-  println("Hello, World!")
+import net.sourceforge.tess4j.Tesseract
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+import java.io.File
+
+object Main {
+  def main(args : Array[String]) = {
+    println("inkプロジェクト始動...")
+    val image = readImageFile("text.jpg") //← TODO: Use sample.jpg and trim that.
+    val text = readText(image)
+    println(text)
+  }
+  
+  def readImageFile(fileName) : BufferedImage =
+    ImageIO.read(new File(fileName))
+
+  def readText(image : BufferedImage) = {
+    val instance = new Tesseract()
+    instance.doOCR(image)
+  }
 }
+
