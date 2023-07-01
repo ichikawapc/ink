ae74b517fdfb25df743fd83e0209fac251eae09c
ringosu <>
2023-07-01 17:16:58 +0900

    feat: implement trimTextSpace

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,6 +14,14 @@ object Main {
   def openImageFile(fileName: String) : BufferedImage =
     ImageIO.read(new File(fileName))
 
+  def trimTextSpace(image : BufferedImage) : BufferedImage = {
+    val width = image.getWidth()
+    val height = 40
+    val x = 0
+    val y = image.getHeight() - height
+    image.getSubimage(x, y, width, height);
+  }
+
   def readText(image : BufferedImage): String = {
     val instance = new Tesseract()
     instance.doOCR(image)
